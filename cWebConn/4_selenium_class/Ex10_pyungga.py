52ffc02a8a5394b58819b1c1e574b696300425b0
Jaedong Kim <jaedong222@naver.com>
2022-12-13 13:49:30 +0900

    파ㅣ이썬임시커밋

---
+++ cWebConn/4_selenium_class/Ex10_pyungga.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+# 1. webdriver 객체생성
+driver = webdriver.Chrome('./webdrive/chromedriver.exe')
+driver.implicitly_wait(3) # 3초 정도 대기 ( 크롬 켜지는 시간 )
+'''
+for page_no in range(1,11):
+driver.get('http://www.cknia.com/store/list.html?page=',page_no)
+'''
+driver.get('http://www.cknia.com/store/list.html')
+
+html = driver.page_source
+# print(html)
+time.sleep(3)
+soup = BeautifulSoup(html,'html.parser')
+
+print(soup)
